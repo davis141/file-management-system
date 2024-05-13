ba239652a4a5c40e4e687c0f272cb0acd5d5017d
davis141 <ohiodavid30@gmail.com>
2024-05-13 10:05:05 +0100

    updated the directory path

---
+++ saas/component/style.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8" />
+<title>FileTrax</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta content="Coderthemes" name="author" />
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+<link rel="stylesheet" href="assets/vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" href="assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css">
+<script src="assets/js/hyper-config.js"></script>
+<link href="assets/css/app-saas.min.css" rel="stylesheet" type="text/css" id="app-style" />
+<link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
